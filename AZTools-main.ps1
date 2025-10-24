c0e2f201de4a1362a7d39ac0376aa3fc26484c12
aalves90 <a.alves@outlook.com>
2025-10-24 09:48:22 -0300

    Update AZTools-main.ps1

--- AZTools-main.ps1
+++ AZTools-main.ps1
@@ -2305,8 +2305,7 @@ function Start-Execution {
     $script:loadingOverlay.Show($form)
 
 
-    $requiredFunctions = @( 'Run-Task', 'Ensure-ChocolateyIsInstalled', 'Install-Chocolatey', 'Test-NetConnectionSafe', 'Install-App', 'Install-Softphone', 'Create-SingleWebShortcut', 'Handle-Hostname', 'Activate-Windows', 'Install-VCRedist', 'Install-DotNetFX', 'Force-WindowsUpdates', 'Configure-System-ThemeAndTaskbar', 'Configure-System-Wallpaper', 'Configure-Teams-Backgrounds', 'Install-OneDrive', 'Disable-Telemetry', 'Adjust-For-Performance', 'Clean-Prefetch', 'Empty-RecycleBin', 'Disk-Cleanup', 'Clean-TempFiles', 'Install-TrendMicroAgent', 'Run-SFC-Scan', 'Run-DISM-Repair', 'Run-Chkdsk', 'Flush-DNS-Cache', 'Reset-Winsock', 'Validate-And-Repair-OneDrive', 'Stop-Process-AndWait', 'Get-Image-List-From-Url', 'Download-File-Robust', 'Find-ExecutablePath', 'Get-DownloadsPath', 'Get-And-Convert-ShortcutIcon', 'Set-TargetReleaseVersion', 'Install-DotNetFX35', 'Install-DotNetFX48', 'Install-DirectX' )
-    $functionDefinitions = @{}
+$requiredFunctions = @( 'Run-Task', 'Ensure-ChocolateyIsInstalled', 'Install-Chocolatey', 'Test-NetConnectionSafe', 'Install-App', 'Install-Softphone', 'Create-SingleWebShortcut', 'Handle-Hostname', 'Activate-Windows', 'Install-VCRedist', 'Install-DotNetFX', 'Force-WindowsUpdates', 'Configure-System-ThemeAndTaskbar', 'Configure-System-Wallpaper', 'Configure-Teams-Backgrounds', 'Install-OneDrive', 'Disable-Telemetry', 'Adjust-For-Performance', 'Clean-Prefetch', 'Empty-RecycleBin', 'Disk-Cleanup', 'Clean-TempFiles', 'Install-TrendMicroAgent', 'Run-SFC-Scan', 'Run-DISM-Repair', 'Run-Chkdsk', 'Flush-DNS-Cache', 'Reset-Winsock', 'Validate-And-Repair-OneDrive', 'Stop-Process-AndWait', 'Get-Image-List-From-Url', 'Download-File-Robust', 'Find-ExecutablePath', 'Get-DownloadsPath', 'Get-And-Convert-ShortcutIcon', 'Set-TargetReleaseVersion', 'Install-DotNetFX35', 'Install-DotNetFX48', 'Install-DirectX', 'Start-ResetWebExperience' )    $functionDefinitions = @{}
     $requiredFunctions | ForEach-Object { if(Test-Path "function:$_") { $functionDefinitions[$_] = (Get-Content "function:$_") } }
     
     $sb = {
@@ -3164,6 +3163,7 @@ $form.Add_Shown({
 Apply-DarkTheme -Control $form
 [void]$form.ShowDialog()
 $form.Dispose()
+
 
 
 
